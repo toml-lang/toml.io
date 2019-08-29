559e5208b45ff1611abf005206089c4b25f334e7
Rob Cameron <rob.cameron@fastmail.com>
2019-08-28 17:12:25 -0700

    Adds JSON support to highlighter

--- assets/javascripts/controllers/snippet_controller.js
+++ assets/javascripts/controllers/snippet_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "stimulus"
 import hljs from 'highlight.js';
 import toml from '../highlight.js/toml';
+import json from 'highlight.js/lib/languages/json';
 
 export default class extends Controller {
   connect() {
     hljs.registerLanguage('toml', toml);
+    hljs.registerLanguage('json', json);
     hljs.highlightBlock(this.element);
   }
 }
