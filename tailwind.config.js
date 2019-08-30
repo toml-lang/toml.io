587f0d748daa977efc26b46e67aa7e237d9abc49
Rob Cameron <rob.cameron@fastmail.com>
2019-08-30 12:39:00 -0700

    Removes several custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontSize: {
-        '7xl': '5rem',
-        '8xl': '6rem'
-      },
-      spacing: {
-        '14': '3.5rem',
-        '72': '18rem',
-        '84': '20rem'
-      }
-    }
-  },
   variants: ['responsive'],
-  plugins: []
 }
