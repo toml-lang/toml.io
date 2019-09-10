a14d21d3f41948e21c25860ecf644bd27a6cc205
Rob Cameron <rob.cameron@fastmail.com>
2019-09-09 17:17:48 -0700

    Have purgecss look in subdirectories

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
     require("autoprefixer"),
     process.env.NODE_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
-        content: ["./public/*.html"],
+        content: ["./public/**/*.html"],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         whitelist: ["tab-active"]
       }),
