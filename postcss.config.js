b5e5e209f8c9e47c22c83b2e4e92907af81844ab
Rob Cameron <rob.cameron@fastmail.com>
2019-09-11 11:02:50 -0700

    Fix postcss-nested, use fork of file-include-webpack-plugin for now

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
-    require("postcss-nesting"),
+    require("postcss-nested"),
     require("autoprefixer"),
     process.env.NODE_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
