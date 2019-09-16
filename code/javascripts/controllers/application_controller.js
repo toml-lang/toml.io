4135cdd70c9083ef3c3a2112f507f7175f56ed2f
Rob Cameron <rob.cameron@fastmail.com>
2019-09-16 10:55:53 -0700

    Adds layout, reorganize directory structure, add locales

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -3,7 +3,46 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+  static get targets() {
+    return ["locale"];
+  }
+
   connect() {
-    console.info("Stimulus Connected.");
+    this.addLocales();
+  }
+
+  changeLocale(event) {
+    let target = event.currentTarget;
+
+    location.href = `/v1/${target.value}`;
+  }
+
+  addLocales() {
+    Object.entries(this.locales).forEach(([value, text]) => {
+      let option = this.optionForLocale(text, value);
+      const regex = `/${value}(\/|$)`;
+      if (location.pathname.match(regex)) {
+        option.selected = true;
+      }
+
+      this.localeTarget.add(option);
+    });
+  }
+
+  optionForLocale(text, value) {
+    let option = document.createElement("option");
+    option.value = value;
+    option.text = text;
+
+    return option;
+  }
+
+  get locales() {
+    return {
+      cn: "🇨🇳 cn",
+      en: "🇺🇸 en",
+      ja: "🇯🇵 ja",
+      ko: "🇰🇷 ko"
+    };
   }
 }
