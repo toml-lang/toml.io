92b4ede2f9bd462d19fd87dbb472aa0de9b1e636
Rob Cameron <rob.cameron@fastmail.com>
2020-05-11 13:26:54 -0700

    Slightly more indent for definition list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /* See https://tailwindcss.com/docs/configuration for more options */
 
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      spacing: {
+        '13': '3.25rem'
+      }
+    }
+  },
   variants: {},
   plugins: []
 };
