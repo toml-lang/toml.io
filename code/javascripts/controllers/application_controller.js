3b91b3f29b18f87a1e3b40ab39246322fbddfd47
Rob Cameron <rob.cameron@fastmail.com>
2020-05-11 16:30:25 -0700

    Generate a latest.html that meta redirects to latest revision

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -7,24 +7,6 @@ export default class extends Controller {
     return ["versions", "languages"];
   }
 
-  connect() {
-    for (let el of document.getElementsByTagName("code")) {
-      if (el.parentElement.nodeName === "PRE") {
-        let container = el.parentElement;
-        let div = document.createElement("div");
-        let link = document.createElement("a");
-
-        div.classList.add("text-right");
-        link.href = "#";
-        link.classList.add("block", "text-right", "text-sm");
-        link.dataset.action = "click->application#copy";
-        link.textContent = "Copy to Clipboard";
-        div.appendChild(link);
-        container.parentElement.insertBefore(div, container.nextSibling);
-      }
-    }
-  }
-
   copy(event) {
     event.preventDefault();
     this.copyContent(event.target.previousSibling.children[0]);
